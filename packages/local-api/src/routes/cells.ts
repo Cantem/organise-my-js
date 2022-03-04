09a3316325523732d4db8c69d9b27bcb9d5f277a
Rafal Porzucek <r.porzucek@gmail.com>
2022-03-04 11:26:12 +0000

    Add json parser

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -10,15 +10,24 @@ interface Cell {
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
+  router.use(express.json());
 
   const fullPath = path.join(dir, filename);
 
   router.get("/cells", async (req, res) => {
-    // make sure the cell storage file exists
-    // if it does not exist, add a default list of cells
-    // Read the file
-    // Parse a list of cells out of it
-    // Send list of cells back to browser
+    try {
+      // Read the file
+      const result = await fs.readFile(fullPath, { encoding: "utf-8" });
+
+      res.send(JSON.parse(result));
+    } catch (err: any) {
+      if (err.code === "ENOENT") {
+        await fs.writeFile(fullPath, "[]", "utf-8");
+        res.send([]);
+      } else {
+        throw err;
+      }
+    }
   });
 
   router.post("/cells", async (req, res) => {
