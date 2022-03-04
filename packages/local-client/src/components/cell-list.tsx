7bb54084f54140a5c2f3d4555ff89a3e613b14c0
Rafal Porzucek <r.porzucek@gmail.com>
2022-03-04 14:48:09 +0000

    Fetch cells

--- packages/local-client/src/components/cell-list.tsx
+++ packages/local-client/src/components/cell-list.tsx
@@ -1,15 +1,22 @@
 import "./cell-list.css";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import { Cell } from "../state";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
+import { useActions } from "../hooks/use-actions";
 
 const CellList: React.FC = () => {
-  // @ts-ignore
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id: string) => data[id])
   );
+
+  const { fetchCells } = useActions();
+
+  useEffect(() => {
+    fetchCells();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const renderedCells = cells.map((cell: Cell) => (
     <Fragment key={cell.id}>
