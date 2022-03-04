c6b5b575d27ef0caf7c0650f206a85b832fb0b63
Rafal Porzucek <r.porzucek@gmail.com>
2022-03-04 11:17:58 +0000

    write cells to file

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import fs from "fs/promises";
+import path from "path";
+
+interface Cell {
+  id: string;
+  content: string;
+  type: "text" | "code";
+}
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
+
+  const fullPath = path.join(dir, filename);
 
   router.get("/cells", async (req, res) => {
     // make sure the cell storage file exists
@@ -12,11 +22,14 @@ export const createCellsRouter = (filename: string, dir: string) => {
   });
 
   router.post("/cells", async (req, res) => {
-    // make sure the file exists
-    // if it does not, create it
     // take list of cells from req obj
     // serialize them
+    const { cells }: { cells: Cell[] } = req.body;
+
     // write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), "utf-8");
+
+    res.send({ status: "ok" });
   });
 
   return router;
